5dccea5b75bdd545489c58edd68df80707cc1c99
Kent Coble <coblekent@gmail.com>
2022-02-15 23:26:57 -0600

    Remove Elasticsearch. Use PostgreSQL. Create sub-directories if missing.

--- export.py
+++ export.py
@@ -1,358 +1,132 @@
 # coding: utf-8
 from argparse import ArgumentParser, Action
+from asyncio import run, set_event_loop_policy, WindowsSelectorEventLoopPolicy
+from asyncpg import connect
 from collections import OrderedDict
 from datetime import datetime, timedelta
-from elasticsearch6 import Elasticsearch
 from json import load, dump
-from os import listdir
+from os import listdir, mkdir
+from platform import system
 
-players_query = {
-    "aggs": {
-        "2": {
-            "date_histogram": {
-                "field": "date",
-                "interval": "1d",
-                "min_doc_count": 0
-            },
-            "aggs": {
-                "3": {
-                    "terms": {
-                        "field": "console.keyword",
-                        "size": 2,
-                        "order": {
-                            "_count": "desc"
-                        },
-                        "min_doc_count": 0
-                    }
-                }
-            }
-        }
-    },
-    "size": 0,
-    "_source": {"excludes": []},
-    "stored_fields": ["*"],
-    "script_fields": {},
-    "docvalue_fields": [
-        {
-            "field": "date",
-            "format": "date_time"
-        }
-    ],
-    "query": {
-        "bool": {
-            "must": [
-                {"match_all": {}},
-                {"match_all": {}},
-                {
-                    "range": {
-                        "date": {
-                            "gte": None,
-                            "lte": None,
-                            "format": "date"
-                        }
-                    }
-                }
-            ],
-            "filter": [],
-            "should": [],
-            "must_not": []
-        }
+
+players_query = '''
+    SELECT count(account_id), console, _date
+    FROM diff_battles
+    WHERE _date >= '{}' AND _date < '{}'
+    GROUP BY console, _date
+    ORDER BY console, _date ASC
+'''
+
+battles_query = '''
+    SELECT sum(battles), console, _date
+    FROM diff_battles
+    WHERE _date >= '{}' AND _date < '{}'
+    GROUP BY console, _date
+    ORDER BY console, _date ASC
+'''
+
+new_players_query = '''
+    SELECT count(account_id), console, created_at::date
+    FROM players
+    WHERE created_at >= '{}' AND created_at < '{}'
+    GROUP BY console, created_at::date
+    ORDER BY console, created_at::date
+'''
+
+five_battles_a_day_query = '''
+    SELECT count(*), console, _date
+    FROM diff_battles
+    WHERE battles >= 5 AND _date >= '{}' AND _date < '{}'
+    GROUP BY console, _date
+    ORDER BY _date, console
+'''
+
+async def query_active_accounts(conn, start, end=datetime.utcnow(), skip=False):
+    dates = [(start + timedelta(days=i)).date() for i in range((end - start).days)]
+    data = {
+        'xbox': OrderedDict((d, 0) for d in dates),
+        'ps': OrderedDict((d, 0) for d in dates)
     }
-}
+    players = await conn.fetch(players_query.format(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')))
 
-battles_query = {
-    "aggs": {
-        "2": {
-            "date_histogram": {
-                "field": "date",
-                "interval": "1d",
-                "min_doc_count": 0
-            },
-            "aggs": {
-                "3": {
-                    "terms": {
-                        "field": "console.keyword",
-                        "size": 2,
-                        "order": {
-                            "1": "desc"
-                        },
-                        "min_doc_count": 0
-                    },
-                    "aggs": {
-                        "1": {
-                            "sum": {
-                                "field": "battles"
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    "size": 0,
-    "_source": {"excludes": []},
-    "stored_fields": ["*"],
-    "script_fields": {},
-    "docvalue_fields": [
-        {
-            "field": "date",
-            "format": "date_time"
-        }
-    ],
-    "query": {
-        "bool": {
-            "must": [
-                {"match_all": {}},
-                {"match_all": {}},
-                {
-                    "range": {
-                        "date": {
-                            "gte": None,
-                            "lte": None,
-                            "format": "date"
-                        }
-                    }
-                }
-            ],
-            "filter": [],
-            "should": [],
-            "must_not": []
-        }
+    for record in players:
+        data[record['console']][record['_date']] = record['count']
+
+    skip_next = False
+    for key, value in data['ps'].items():
+        if skip and value < 20000:
+            skip_next = True
+            continue
+        elif skip and skip_next:
+            skip_next = False
+            continue
+
+        with open('data/summary/active/' + key.strftime('%Y-%m-%d') + '.json', 'w') as f:
+            dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
+
+
+async def query_battles(conn, start, end=datetime.utcnow(), skip=False):
+    dates = [(start + timedelta(days=i)).date() for i in range((end - start).days)]
+    data = {
+        'xbox': OrderedDict((d, 0) for d in dates),
+        'ps': OrderedDict((d, 0) for d in dates)
     }
-}
+    battles = await conn.fetch(battles_query.format(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')))
 
-new_players_query = {
-    "aggs": {
-        "2": {
-            "date_histogram": {
-                "field": "created_at",
-                "interval": "1d",
-                "min_doc_count": 0
-            },
-            "aggs": {
-                "3": {
-                    "terms": {
-                        "field": "console.keyword",
-                        "size": 2,
-                        "order": {
-                            "_count": "desc"
-                        },
-                        "min_doc_count": 0
-                    }
-                }
-            }
-        }
-    },
-    "size": 0,
-    "_source": {"excludes": []},
-    "stored_fields": ["*"],
-    "script_fields": {},
-    "docvalue_fields": [
-        {
-            "field": "created_at",
-            "format": "date_time"
-        }
-    ],
-    "query": {
-        "bool": {
-            "must": [
-                {"match_all": {}},
-                {"match_all": {}},
-                {
-                    "range": {
-                        "created_at": {
-                            "gte": None,
-                            "lt": None,
-                            "format": "date"
-                        }
-                    }
-                }
-            ],
-            "filter": [],
-            "should": [],
-            "must_not": []
-        }
+    for record in battles:
+        data[record['console']][record['_date']] = record['sum']
+
+    skip_next = False
+    for key, value in data['ps'].items():
+        if skip and value < 400000:
+            skip_next = True
+            continue
+        elif skip and skip_next:
+            skip_next = False
+            continue
+
+        with open('data/summary/battles/' + key.strftime('%Y-%m-%d') + '.json', 'w') as f:
+            dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
+
+
+async def query_new_players(conn, start, end=datetime.utcnow()):
+    dates = [(start + timedelta(days=i)).date() for i in range((end - start).days)]
+    data = {
+        'xbox': OrderedDict((d, 0) for d in dates),
+        'ps': OrderedDict((d, 0) for d in dates)
     }
-}
+    new_players = await conn.fetch(new_players_query.format(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')))
 
-five_battles_a_day_query = {
-    'aggs': {
-        '4': {
-            'date_histogram': {
-                'field': 'date',
-                'interval': '1d',
-                'min_doc_count': 0
-            },
-            'aggs': {
-                '3': {
-                    'terms': {
-                        'field': 'console.keyword',
-                        'size': 2,
-                        'order': {'_count': 'desc'}
-                    },
-                    'aggs': {
-                        '2': {
-                            'range': {
-                                'field': 'battles',
-                                'ranges': [{'from': 5, 'to': None}],
-                                'keyed': True
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    'size': 0,
-    '_source': {'excludes': []},
-    'stored_fields': ['*'],
-    'script_fields': {},
-    'docvalue_fields': [{'field': 'date', 'format': 'date_time'}],
-    'query': {
-        'bool': {
-            'must': [
-                {'match_all': {}},
-                {'match_all': {}},
-                {
-                    'range': {
-                        'date': {
-                            'gte': None,
-                            'lte': None,
-                            'format': 'date'
-                        }
-                    }
-                }
-            ],
-            'filter': [],
-            'should': [],
-            'must_not': []
-        }
+    for record in new_players:
+        data[record['console']][record['created_at']] = record['count']
+
+    for key in data['ps']:
+        with open('data/summary/new/' + key.strftime('%Y-%m-%d') + '.json', 'w') as f:
+            dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
+
+
+async def query_min_5_a_day(conn, start, end=datetime.utcnow(), skip=False):
+    dates = [(start + timedelta(days=i)).date() for i in range((end - start).days)]
+    data = {
+        'xbox': OrderedDict((d, 0) for d in dates),
+        'ps': OrderedDict((d, 0) for d in dates)
     }
-}
+    five_battles = await conn.fetch(five_battles_a_day_query.format(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')))
 
-def timerange(start, end=datetime.utcnow(), step=90):
-    delta = timedelta(days=step)
-    next_iter = start
-    while end > next_iter:
-        yield (next_iter, next_iter + delta)
-        next_iter = next_iter + delta
+    for record in five_battles:
+        data[record['console']][record['_date']] = record['count']
 
+    skip_next = False
+    for key, value in data['ps'].items():
+        if skip and value < 20000:
+            skip_next = True
+            continue
+        elif skip and skip_next:
+            skip_next = False
+            continue
 
-def query_active_accounts(elastic, index, start, end=datetime.utcnow(), step=90, skip=False):
-    data = {'xbox': OrderedDict(), 'ps': OrderedDict()}
-    tr = timerange(start, end, step)
-    for older, newer in tr:
-        players_query['query']['bool'][
-                'must'][-1]['range']['date']['gte'] = older.strftime('%Y-%m-%d')
-        players_query['query']['bool'][
-                'must'][-1]['range']['date']['lte'] = newer.strftime('%Y-%m-%d')
-        players = elastic.search(index=index, body=players_query)
-
-        for bucket in players['aggregations']['2']['buckets']:
-            key = bucket['key_as_string'].split('T')[0]
-            data['xbox'][key] = 0
-            data['ps'][key] = 0
-            if not bucket['3']['buckets']:
-                continue
-            for subbucket in bucket['3']['buckets']:
-                data[subbucket['key']][key] = subbucket['doc_count']
-
-        skip_next = False
-        for key, value in data['ps'].items():
-            if skip and value < 20000:
-                skip_next = True
-                continue
-            elif skip and skip_next:
-                skip_next = False
-                continue
-            # Gotta convert the time to represent the day of activity, not the day the collection ran
-            with open('data/summary/active/' + (datetime.strptime(key, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d') + '.json', 'w') as f:
-                dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
-
-
-def query_battles(elastic, index, start, end=datetime.utcnow(), step=90, skip=False):
-    data = {'xbox': OrderedDict(), 'ps': OrderedDict()}
-    tr = timerange(start, end, step)
-    for older, newer in tr:
-        battles_query['query']['bool'][
-                'must'][-1]['range']['date']['gte'] = older.strftime('%Y-%m-%d')
-        battles_query['query']['bool'][
-                'must'][-1]['range']['date']['lte'] = newer.strftime('%Y-%m-%d')
-        battles = elastic.search(index=index, body=battles_query)
-
-        for bucket in battles['aggregations']['2']['buckets']:
-            key = bucket['key_as_string'].split('T')[0]
-            data['xbox'][key] = 0
-            data['ps'][key] = 0
-            if not bucket['3']['buckets']:
-                continue
-            for subbucket in bucket['3']['buckets']:
-                data[subbucket['key']][key] = subbucket['1']['value']
-
-        skip_next = False
-        for key, value in data['ps'].items():
-            if skip and value < 400000:
-                skip_next = True
-                continue
-            elif skip and skip_next:
-                skip_next = False
-                continue
-            # Gotta convert the time to represent the day of activity, not the day the collection ran
-            with open('data/summary/battles/' + (datetime.strptime(key, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d') + '.json', 'w') as f:
-                dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
-
-
-def query_new_players(elastic, index, start, end=datetime.utcnow(), step=90):
-    tr = timerange(start, end, step)
-    for older, newer in tr:
-        new_players_query['query']['bool'][
-                'must'][-1]['range']['created_at']['gte'] = older.strftime('%Y-%m-%d')
-        new_players_query['query']['bool'][
-                'must'][-1]['range']['created_at']['lt'] = newer.strftime('%Y-%m-%d')
-        new_players = elastic.search(index=index, body=new_players_query)
-
-        for bucket in new_players['aggregations']['2']['buckets']:
-            key = bucket['key_as_string'].split('T')[0]
-            if not bucket['3']['buckets']:
-                continue
-            data = {}
-            for subbucket in bucket['3']['buckets']:
-                data[subbucket['key']] = subbucket['doc_count']
-            # No time conversion. Timestamps are provided by WG
-            with open('data/summary/new/' + key + '.json', 'w') as f:
-                dump(data, f)
-
-
-def query_min_5_a_day(elastic, index, start, end=datetime.utcnow(), step=90, skip=False):
-    data = {'xbox': OrderedDict(), 'ps': OrderedDict()}
-    tr = timerange(start, end, step)
-    for older, newer in tr:
-        five_battles_a_day_query['query']['bool'][
-                'must'][-1]['range']['date']['gte'] = older.strftime('%Y-%m-%d')
-        five_battles_a_day_query['query']['bool'][
-                'must'][-1]['range']['date']['lte'] = newer.strftime('%Y-%m-%d')
-        five_battles = elastic.search(index=index, body=five_battles_a_day_query)
-
-        for bucket in five_battles['aggregations']['4']['buckets']:
-            key = bucket['key_as_string'].split('T')[0]
-            data['xbox'][key] = 0
-            data['ps'][key] = 0
-            if not bucket['3']['buckets']:
-                continue
-            for subbucket in bucket['3']['buckets']:
-                data[subbucket['key']][key] = subbucket['2']['buckets']['5.0-*']['doc_count']
-
-        skip_next = False
-        for key, value in data['ps'].items():
-            if skip and value < 20000:
-                skip_next = True
-                continue
-            elif skip and skip_next:
-                skip_next = False
-                continue
-            # Gotta convert the time to represent the day of activity, not the day the collection ran
-            with open('data/summary/min5/' + (datetime.strptime(key, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d') + '.json', 'w') as f:
-                dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
+        with open('data/summary/min5/' + key.strftime('%Y-%m-%d') + '.json', 'w') as f:
+            dump({'xbox': data['xbox'][key], 'ps': data['ps'][key]}, f)
 
 
 def create_file_listings():
@@ -365,6 +139,28 @@ def create_file_listings():
     min5 = listdir('data/summary/min5/')
     with open('data/available/min5.txt', 'w') as f:
         f.writelines(map(lambda l: l.split('.')[0] + '\n', sorted(min5)))
+
+
+def setup_directories():
+    dirs = (  # Order-dependent!
+        'data', 'data/available', 'data/summary', 'data/summary/active',
+        'data/summary/battles', 'data/summary/new', 'data/summary/min5'
+    )
+    for d in dirs:
+        try:
+            mkdir(d)
+        except FileExistsError:
+            pass
+
+
+async def export_data(config, args):
+    setup_directories()
+    db = await connect(**config['database'])
+    await query_active_accounts(db, args.start, args.end, args.skip)
+    await query_battles(db, args.start, args.end, args.skip)
+    await query_new_players(db, args.start, args.end)
+    await query_min_5_a_day(db, args.start, args.end, args.skip)
+    create_file_listings()
 
 
 class ConvertTime(Action):
@@ -384,7 +180,6 @@ if __name__ == '__main__':
     agp.add_argument('-s', '--start', default=datetime(2014, 1, 1, 0, 0), action=ConvertTime, help='%%Y-%%m-%%d format')
     agp.add_argument('-c', '--config', default='config.json')
     agp.add_argument('-e', '--end', default=datetime.utcnow(), action=ConvertTime, help='%%Y-%%m-%%d format')
-    agp.add_argument('--step', default=90, type=int)
     agp.add_argument('--skip', action='store_true')
 
     args = agp.parse_args()
@@ -392,10 +187,6 @@ if __name__ == '__main__':
     with open(args.config) as f:
         config = load(f)
 
-    es = Elasticsearch(**config['elasticsearch'])
-
-    query_active_accounts(es, config['es index'], args.start, args.end, args.step, args.skip)
-    query_battles(es, config['es index'], args.start, args.end, args.step, args.skip)
-    query_new_players(es, 'players', args.start, args.end, args.step)
-    query_min_5_a_day(es, config['es index'], args.start, args.end, args.step, args.skip)
-    create_file_listings()
+    if system() == 'Windows':
+        set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+    run(export_data(config, args))
